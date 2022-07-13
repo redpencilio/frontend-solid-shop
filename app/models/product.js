e174c07acfe1a43d646f595625ed4ce498745d05
Aad Versteden <madnificent@gmail.com>
2022-07-13 12:58:05 +0200

    Fix product model

--- app/models/product.js
+++ app/models/product.js
@@ -2,19 +2,17 @@ import SemanticModel, {
   solid,
   string,
 } from 'ember-solid/models/semantic-model';
-import { rdfType } from 'ember-solid/models/semantic-model';
 
 @solid({
   defaultStorageLocation: '/private/tests/my-products.ttl', // default location in solid pod
   private: true, // is this private info for the user?
-  rdfType: 'ProductOrService', // smessie: did rdfType here instead of type
+  type: 'http://purl.org/goodrelations/ProductOrService', // smessie: did rdfType here instead of type
   ns: 'http://purl.org/goodrelations/',
 })
-@rdfType('http://purl.org/goodrelations/ProductOrService') // smessie: tried to add this here
 export default class Product extends SemanticModel {
-  @string
+  @string()
   name;
 
-  @string
+  @string()
   description;
 }
