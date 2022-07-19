a0932c628cedd5a45e5a0b69d1be06361a13897c
smessie <smessie@smessie.com>
2022-07-19 10:16:14 +0200

    feat: Implement search parameters

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,13 +1,30 @@
 import Route from '@ember/routing/route';
 
 export default class IndexRoute extends Route {
-  async model() {
-    const result = await fetch('/query', {
-      method: 'GET',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-    });
+  queryParams = {
+    name: {
+      refreshModel: true,
+    },
+    description: {
+      refreshModel: true,
+    },
+    seller: {
+      refreshModel: true,
+    },
+  };
+
+  async model({ name, description, seller }) {
+    const result = await fetch(
+      `/query?name=${name || ''}&description=${description || ''}&seller=${
+        seller || ''
+      }`,
+      {
+        method: 'GET',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      }
+    );
     const json = await result.json();
     return json.results.bindings;
   }
